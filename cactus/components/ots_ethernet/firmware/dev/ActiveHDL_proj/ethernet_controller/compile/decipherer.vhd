6bef3991d7501bfd6454ae0c47c1f37f10f8ef25
rrivera <rrivera@fnal.gov>
2016-05-17 17:35:07 -0500

    Apparently a bug was introduced in selecting output info fifo in tx_seq_ctl.. fixed bug. Eased routing in Decipherer. Added to sim waveforms to git.

--- cactus/components/ots_ethernet/firmware/dev/ActiveHDL_proj/ethernet_controller/compile/decipherer.vhd
+++ cactus/components/ots_ethernet/firmware/dev/ActiveHDL_proj/ethernet_controller/compile/decipherer.vhd
@@ -8,7 +8,7 @@
 -------------------------------------------------------------------------------
 --
 -- File        : d:\Projects\otsdaq\OtS Ethernet MAC firmware\ActiveHDL_proj\ethernet_controller\compile\decipherer.vhd
--- Generated   : 02/29/16 11:09:14
+-- Generated   : 05/17/16 16:44:53
 -- From        : d:/Projects/otsdaq/OtS Ethernet MAC firmware/ActiveHDL_proj/ethernet_controller/src/decipherer.asf
 -- By          : FSM2VHDL ver. 5.0.7.2
 --
@@ -79,6 +79,7 @@ signal is_udp_sig: STD_LOGIC;
 signal udp_countdown: STD_LOGIC_VECTOR (15 downto 0);
 signal udp_data_valid_sig: STD_LOGIC;
 signal udp_dest_ip: STD_LOGIC_VECTOR (31 downto 0);
+signal udp_dest_ip_reg: STD_LOGIC_VECTOR (31 downto 0);
 signal udp_dest_port: STD_LOGIC_VECTOR (15 downto 0);
 signal udp_zeros: STD_LOGIC_VECTOR (10 downto 0);
 
@@ -193,7 +194,9 @@ match_proc : process(clk)
 begin
 if rising_edge(clk) then
 	addrs_match_sig <= '0';
-	if (udp_dest_ip = self_addrs) then -- and
+	udp_dest_ip_reg <= udp_dest_ip;
+	--add register to help meet timing
+	if (udp_dest_ip_reg = self_addrs) then -- and
 --(self_port = 0 or udp_dest_port = self_port)) then
 -- Note: rejecting the port presented a problem for ICMP matching logic
 --(x"C0A885" & addrs) then --this UDP packet was intended for this firmware.
