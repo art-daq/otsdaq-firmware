dd154cabf31bff6282e20d81d8efbfdb34362d19
rrivera <rrivera@fnal.gov>
2016-07-27 16:20:22 -0500

    checking in function declarations for DatabaseConfigurationInterface

--- cactus/components/ots_ethernet/firmware/hdl/params_package.vhd
+++ cactus/components/ots_ethernet/firmware/hdl/params_package.vhd
@@ -27,7 +27,7 @@ package params_package is
 	constant delay_term: natural := 0; --x"000A" --x"07D0";
 
 	-- DO NOT TOUCH IP ADDRESS LINE BELOW.. Managed by setup and install script!
-	constant ETH_CONTROLLER_DEFAULT_ADDR: std_logic_vector(7 downto 0) := std_logic_vector(to_unsigned(36,8)); -- must be 1 to 254 inclusive		
+	constant ETH_CONTROLLER_DEFAULT_ADDR: std_logic_vector(7 downto 0) := std_logic_vector(to_unsigned(2,8)); -- must be 1 to 254 inclusive		
 	constant ETH_CONTROLLER_DEFAULT_PORT: std_logic_vector(15 downto 0) := std_logic_vector(to_unsigned(2007,16)); -- must be 0 to 65535 inclusive
 	-- DO NOT TOUCH IP ADDRESS LINE ABOVE.. Managed by setup and install script!
 	
