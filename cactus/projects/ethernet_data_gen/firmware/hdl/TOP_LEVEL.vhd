aef6eed65e44dd0a03287e0c7c57ab498ece8306
rrivera <rrivera@fnal.gov>
2017-03-14 13:33:15 -0500

    checkpoint - progress on table descriptions and fixed choices. Not yet displayed in config gui.

--- cactus/projects/ethernet_data_gen/firmware/hdl/TOP_LEVEL.vhd
+++ cactus/projects/ethernet_data_gen/firmware/hdl/TOP_LEVEL.vhd
@@ -15,10 +15,10 @@ entity top is
 	PHY_RXD1	: in    std_logic; 
 	PHY_RXD2	: in    std_logic; 
 	PHY_RXD3	: in    std_logic; 
---	PHY_RXD4	: in    std_logic; 
---	PHY_RXD5	: in    std_logic; 
---	PHY_RXD6	: in    std_logic; 
---	PHY_RXD7	: in    std_logic; 
+	PHY_RXD4	: in    std_logic; 
+	PHY_RXD5	: in    std_logic; 
+	PHY_RXD6	: in    std_logic; 
+	PHY_RXD7	: in    std_logic; 
 		  
 		--PHY_RXER     : in    std_logic; 
 		--USER_CLOCK   : in    std_logic; 
@@ -30,11 +30,11 @@ entity top is
 	PHY_TXD1	: out   std_logic; 
 	PHY_TXD2	: out   std_logic; 
 	PHY_TXD3	: out   std_logic;
---	PHY_TXD4	: out   std_logic; 
---	PHY_TXD5	: out   std_logic; 
---	PHY_TXD6	: out   std_logic; 
---	PHY_TXD7	: out   std_logic; 
---	PHY_TXER	: out   std_logic;
+	PHY_TXD4	: out   std_logic; 
+	PHY_TXD5	: out   std_logic; 
+	PHY_TXD6	: out   std_logic; 
+	PHY_TXD7	: out   std_logic; 
+	PHY_TXER	: out   std_logic;
 						  
 		PHY_TXC_GTXCLK : out   std_logic
           
@@ -101,8 +101,8 @@ architecture BEHAVIORAL of top is
     attribute mark_debug of b_data : signal is "true";
     attribute mark_debug of b_data_we : signal is "true";
     
-	--    attribute mark_debug of GMII_RXD_0_sig : signal is "true";
-	--    attribute mark_debug of GMII_RX_DV_0_sig : signal is "true";
+	attribute mark_debug of GMII_RXD_0_sig : signal is "true";
+	attribute mark_debug of GMII_RX_DV_0_sig : signal is "true";
        
    
    
@@ -294,12 +294,12 @@ begin
     
      IBUF_PHY_RXDV : IBUF       port map (I=>PHY_RXCTL_RXDV,  O=>GMII_RX_DV_0_sig);
       
-	GMII_RXD_0_sig(7 downto 4) <= (others => '0'); -- for RGMII or SGMII
+--removed by script (others => '0'); -- for RGMII or SGMII
      
---	IBUF_PHY_RXD7 : IBUF        port map (I=>PHY_RXD7, O=>GMII_RXD_0_sig(7));	  
---	IBUF_PHY_RXD6 : IBUF        port map (I=>PHY_RXD6, O=>GMII_RXD_0_sig(6));	  
---	IBUF_PHY_RXD5 : IBUF        port map (I=>PHY_RXD5, O=>GMII_RXD_0_sig(5)); 	  
---	IBUF_PHY_RXD4 : IBUF        port map (I=>PHY_RXD4, O=>GMII_RXD_0_sig(4));
+	IBUF_PHY_RXD7 : IBUF        port map (I=>PHY_RXD7, O=>GMII_RXD_0_sig(7));	  
+	IBUF_PHY_RXD6 : IBUF        port map (I=>PHY_RXD6, O=>GMII_RXD_0_sig(6));	  
+	IBUF_PHY_RXD5 : IBUF        port map (I=>PHY_RXD5, O=>GMII_RXD_0_sig(5)); 	  
+	IBUF_PHY_RXD4 : IBUF        port map (I=>PHY_RXD4, O=>GMII_RXD_0_sig(4));
 	IBUF_PHY_RXD3 : IBUF        port map (I=>PHY_RXD3, O=>GMII_RXD_0_sig(3));
 	IBUF_PHY_RXD2 : IBUF        port map (I=>PHY_RXD2, O=>GMII_RXD_0_sig(2));
 	IBUF_PHY_RXD1 : IBUF        port map (I=>PHY_RXD1, O=>GMII_RXD_0_sig(1));
@@ -307,23 +307,23 @@ begin
      
      GMII_RX_ER_0_sig <= '0';
      
-     IBUF_PHY_RXCLK : BUFG      port map (I=>PHY_RXCLK,  O=>MASTER_CLK);
+     IBUF_PHY_RXCLK : IBUFG      port map (I=>PHY_RXCLK,  O=>MASTER_CLK);
         
     -----------------------
     ----------------------- OBUF 's 
     	 
 	OBUF_PHY_RESET : OBUF	   port map (I=>'1',  O=>PHY_RESET); --hold not reset
 		 
---	OBUF_PHY_TXER : OBUF       port map (I=>PHY_TXER_sig,  O=>PHY_TXER);
+	OBUF_PHY_TXER : OBUF       port map (I=>PHY_TXER_sig,  O=>PHY_TXER);
 	 
     OBUF_PHY_TXEN : OBUF       port map (I=>PHY_TXEN_sig,  O=>PHY_TXCTL_TXEN);
     
     OBUF_PHY_TXCLK : OBUF      port map (I=>GTX_CLK_0_sig, O=>PHY_TXC_GTXCLK);
     
---	OBUF_PHY_TXD7 : OBUF   	   port map (I=>PHY_TXD_sig(7), O=>PHY_TXD7);    
---	OBUF_PHY_TXD6 : OBUF       port map (I=>PHY_TXD_sig(6), O=>PHY_TXD6);    
---	OBUF_PHY_TXD5 : OBUF       port map (I=>PHY_TXD_sig(5), O=>PHY_TXD5);    
---	OBUF_PHY_TXD4 : OBUF       port map (I=>PHY_TXD_sig(4), O=>PHY_TXD4);    
+	OBUF_PHY_TXD7 : OBUF   	   port map (I=>PHY_TXD_sig(7), O=>PHY_TXD7);    
+	OBUF_PHY_TXD6 : OBUF       port map (I=>PHY_TXD_sig(6), O=>PHY_TXD6);    
+	OBUF_PHY_TXD5 : OBUF       port map (I=>PHY_TXD_sig(5), O=>PHY_TXD5);    
+	OBUF_PHY_TXD4 : OBUF       port map (I=>PHY_TXD_sig(4), O=>PHY_TXD4);    
 	OBUF_PHY_TXD3 : OBUF       port map (I=>PHY_TXD_sig(3), O=>PHY_TXD3);    
 	OBUF_PHY_TXD2 : OBUF       port map (I=>PHY_TXD_sig(2), O=>PHY_TXD2);    
 	OBUF_PHY_TXD1 : OBUF       port map (I=>PHY_TXD_sig(1), O=>PHY_TXD1);    
