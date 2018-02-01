a52bafc8b7c76c8ac8bab78bb56289e67ede6e40
Lorenzo Uplegger <uplegger@fnal.gov>
2018-02-01 15:47:17 -0600

    nim+ and nim+X both meet timing with master_clk chipscope in 4-5 minutes

--- cactus/projects/nim_plus_x/FW_Transfer_2018_01_24/sig_gen_prog.vhd
+++ cactus/projects/nim_plus_x/FW_Transfer_2018_01_24/sig_gen_prog.vhd
@@ -7,9 +7,9 @@
 --
 -------------------------------------------------------------------------------
 --
--- File        : c:\NIM_Plus\AGP_2016\NIMPlus\NIMPlus\compile\sig_gen_prog.vhd
--- Generated   : 03/31/17 14:33:53
--- From        : c:\NIM_Plus\AGP_2016\NIMPlus\NIMPlus\src\sig_gen_prog.asf
+-- File        : U:\PREP\PREP_Modernization\Firmware_Backups\Aldec_Backups\One_Phase_Designs\AGP_2018_01_30_NIMPlus_jw121_320MHz_1Phase_Accel_Sync\NIMPlus\NIMPlus\compile\sig_gen_prog.vhd
+-- Generated   : 02/01/18 14:53:31
+-- From        : U:\PREP\PREP_Modernization\Firmware_Backups\Aldec_Backups\One_Phase_Designs\AGP_2018_01_30_NIMPlus_jw121_320MHz_1Phase_Accel_Sync\NIMPlus\NIMPlus\src\sig_gen_prog.asf
 -- By          : FSM2VHDL ver. 5.0.7.2
 --
 -------------------------------------------------------------------------------
@@ -26,7 +26,7 @@ use IEEE.std_logic_unsigned.all;
 entity sig_gen_prog is 
 	port (
 		clk: in STD_LOGIC;
-		p_count: in STD_LOGIC_VECTOR (31 downto 0);
+		p_count: in STD_LOGIC_VECTOR (27 downto 0);
 		p_hi: in STD_LOGIC_VECTOR (31 downto 0);
 		p_lo: in STD_LOGIC_VECTOR (31 downto 0);
 		reset_p: in STD_LOGIC;
@@ -37,10 +37,12 @@ end sig_gen_prog;
 
 architecture sig_gen_prog_arch of sig_gen_prog is
 
+constant u_28: STD_LOGIC_VECTOR (27 downto 0) := "0000000000000000000000000001";
 constant u_32: STD_LOGIC_VECTOR (31 downto 0) := "00000000000000000000000000000001";
+constant z_28: STD_LOGIC_VECTOR (27 downto 0) := "0000000000000000000000000000";
 constant z_32: STD_LOGIC_VECTOR (31 downto 0) := "00000000000000000000000000000000";
 -- diagram signals declarations
-signal p_count_r: STD_LOGIC_VECTOR (31 downto 0);
+signal p_count_r: STD_LOGIC_VECTOR (27 downto 0);
 signal p_hi_r: STD_LOGIC_VECTOR (31 downto 0);
 signal p_lo_r: STD_LOGIC_VECTOR (31 downto 0);
 
@@ -55,7 +57,7 @@ signal Sreg0, NextState_Sreg0: Sreg0_type;
 -- Declarations of pre-registered internal signals
 signal int_err_out, next_err_out: STD_LOGIC;
 signal int_p_out, next_p_out: STD_LOGIC;
-signal next_p_count_r: STD_LOGIC_VECTOR (31 downto 0);
+signal next_p_count_r: STD_LOGIC_VECTOR (27 downto 0);
 signal next_p_hi_r: STD_LOGIC_VECTOR (31 downto 0);
 signal next_p_lo_r: STD_LOGIC_VECTOR (31 downto 0);
 
@@ -79,16 +81,16 @@ begin
 	next_p_lo_r <= p_lo_r;
 	case Sreg0 is
 		when S1 =>
-			if start_en = '1' then
-				NextState_Sreg0 <= S2;
-				next_p_count_r <= z_32;
-				next_p_hi_r <= z_32;
-				next_p_lo_r <= z_32;
-				next_p_out <= '0';
-			elsif start_en = '0' then
+			if start_en = '0' then
 				NextState_Sreg0 <= S1;
 				next_p_out <= '0';
 				next_err_out <= '0';
+			elsif start_en = '1' then
+				NextState_Sreg0 <= S2;
+				next_p_count_r <= z_28;
+				next_p_hi_r <= z_32;
+				next_p_lo_r <= z_32;
+				next_p_out <= '0';
 			end if;
 		when S2 =>
 			if start_en = '0' then
@@ -103,43 +105,43 @@ begin
 				NextState_Sreg0 <= S4;
 			end if;
 		when S3 =>
-			if start_en = '0' then
-				NextState_Sreg0 <= S1;
-				next_p_out <= '0';
-				next_err_out <= '0';
-			elsif start_en = '1' then
+			if start_en = '1' then
 				NextState_Sreg0 <= S3;
 				next_err_out <= '1';
 				next_p_out <= '0';
+			elsif start_en = '0' then
+				NextState_Sreg0 <= S1;
+				next_p_out <= '0';
+				next_err_out <= '0';
 			end if;
 		when S4 =>
-			if p_hi_r = p_hi then
-				NextState_Sreg0 <= S5;
-				next_p_out <= '0';
-			elsif p_hi_r < p_hi then
+			if p_hi_r < p_hi then
 				NextState_Sreg0 <= S4;
 				next_p_hi_r <= p_hi_r + u_32;
 				next_p_out <= '1';
+			elsif p_hi_r = p_hi then
+				NextState_Sreg0 <= S5;
+				next_p_out <= '0';
 			end if;
 		when S5 =>
 			if p_lo_r = p_lo then
 				NextState_Sreg0 <= S7;
-				next_p_count_r <= p_count_r + u_32;
+				next_p_count_r <= p_count_r + u_28;
 			elsif p_lo_r < p_lo then
 				NextState_Sreg0 <= S5;
 				next_p_out <= '0';
 				next_p_lo_r <= p_lo_r + u_32;
 			end if;
 		when S7 =>
-			if p_count_r = p_count and 
-				p_count /= z_32 then
-				NextState_Sreg0 <= S8;
-				next_p_out <= '0';
-			elsif (p_count_r < p_count and p_count /= z_32)
-				or p_count = z_32 then
+			if (p_count_r < p_count and p_count /= z_28)
+				or p_count = z_28 then
 				NextState_Sreg0 <= S2;
 				next_p_hi_r <= z_32;
 				next_p_lo_r <= z_32;
+			elsif p_count_r = p_count and 
+				p_count /= z_28 then
+				NextState_Sreg0 <= S8;
+				next_p_out <= '0';
 			end if;
 		when S8 =>
 			if start_en = '0' then
