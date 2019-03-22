01b8c14bf1121191a6ff218597cd7b91bd8e5589
Ryan Rivera <rrivera@fnal.gov>
2019-03-22 15:44:09 -0500

    added commented out MCS and Flash constraints

--- cactus/boards/kc705/firmware/ucf/pins.tcl
+++ cactus/boards/kc705/firmware/ucf/pins.tcl
@@ -18,6 +18,11 @@
 set_property IOSTANDARD LVCMOS25 [get_ports PHY_RXCLK]
 set_property PACKAGE_PIN U27 [get_ports PHY_RXCLK]
 set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets MASTER_CLK]
+										  
+
+#66 MHz EMCCLK flash clock
+set_property IOSTANDARD LVCMOS25 [get_ports USER_CLOCK]
+set_property PACKAGE_PIN R24 [get_ports USER_CLOCK]							  
 
 set_property PACKAGE_PIN R28 [get_ports PHY_RXCTL_RXDV]
 set_property IOSTANDARD LVCMOS25 [get_ports PHY_RXCTL_RXDV]
