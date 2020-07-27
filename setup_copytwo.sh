047eb2486ab1917751fe1abef11ef9a6e5b3d4fd
Ryan Rivera <rrivera@fnal.gov>
2020-07-27 15:48:10 -0500

    Iris created copy script for ease of copying bitfiles, mcs, and debug files with a single call and time stamp edit

--- setup_copytwo.sh
+++ setup_copytwo.sh
@@ -11,13 +11,13 @@
 function copytwo {
   # Bash has weird variable rules because of course it does, use "local"
   # these are really just constants to make it clearer
-  local remote_host="$2"#"rulinux03.dhcp.fnal.gov"
+  local remote_host="$2" #"rulinux03.dhcp.fnal.gov"
   
   # If really the only changing thing is the timestamp we just take the timestamp as the first argument which is in $1
   # Everything is quoted because I don't know, it makes me feel better
-  local source_prefix="$1"#"captan_plus_ddcp/top/top.runs/impl_1/top"
-  local destination_prefix="$3"#"pipii_ddcp_bitfiles/captan_plus_bare_ddcp_"
-  local destination_suffix="$4"#"$1"
+  local source_prefix="$1"  #"captan_plus_ddcp/top/top.runs/impl_1/top"
+  local destination_prefix="$3"  #"pipii_ddcp_bitfiles/captan_plus_bare_ddcp_"
+  #local destination_suffix="$4"#"$1"
   
   # there are multiple ways to define the set of extensions we use
   # I think it's easiest to use a proper array which is setup like this (note the use of spaces)
@@ -29,7 +29,8 @@ function copytwo {
   do
     # ${varname} is safe even if varname has spaces or would bleed into the rest of the line
     # double-quotes allow spaces in the filename, I use both for consistency
-    scp "${source_prefix}${ext}" "${remote_host}:${destination_prefix}${destination_suffix}${ext}"
+     #scp "${source_prefix}${ext}" "${remote_host}:${destination_prefix}${destination_suffix}${ext}"
+    scp "${source_prefix}${ext}" "${remote_host}:${destination_prefix}${ext}"
   done
 } # end of copytwo
 
